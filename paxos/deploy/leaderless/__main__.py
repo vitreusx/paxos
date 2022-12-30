6f1d0b822f130de60528b1c487a21d24297da877
vitreusx <jakub_bednarz@protonmail.com>
2022-12-30 21:49:22 +0100

    Rewrote leaderless deployment Removed old versions

--- paxos/deploy/leaderless/__main__.py
+++ paxos/deploy/leaderless/__main__.py
@@ -1,24 +1,20 @@
 import argparse
 import logging
-from ..sockets import *
-from typing import List
-import scipy.stats
-from ..killer import Killer
-from ..worker import PaxosWorker
-import tempfile
-from pathlib import Path
-import jinja2
+import signal
 import subprocess
+import tempfile
+import threading
+from pathlib import Path
 from subprocess import DEVNULL
 import os
-import signal
-from flask import Flask, request
-from marshmallow import Schema, fields
-import threading
-import sys
-from multiprocessing import Process
+from typing import List
+import jinja2
+from ..killer import Killer
+from ..worker import PaxosWorker
+from ..sockets import *
+import scipy
 
-class WithLeader:
+class Leaderless:
     def parse_args(self):
         p = argparse.ArgumentParser()
 
@@ -35,16 +31,17 @@ class WithLeader:
             metavar=("MEAN", "MAX_DEV"),
         )
         p.add_argument("--ledger-file", required=True)
-        p.add_argument("--prober-port", type=int)
-        p.add_argument("--probe-period", type=float, required=True)
-        p.add_argument("--num-workers", type=int)
+
+        g = p.add_mutually_exclusive_group()
+        g.add_argument("--num-workers", type=int)
+
         p.add_argument("--gateway-port", type=int)
+
         p.add_argument("-v", "--verbose", action="store_true")
 
         return p.parse_args()
-
+    
     def setup_logging(self):
-        # Set up the logger - by default INFO-level stuff is suppressed.
         logging.getLogger("werkzeug").setLevel(logging.WARN)
         level = logging.INFO if self.args.verbose else logging.WARN
         logging.basicConfig(level=level)
@@ -55,14 +52,14 @@ class WithLeader:
                 sset.reserve(port=self.args.gateway_port)
                 _, self.flask_port = sset.reserve()
             
-            _, self.prober_port = sset.reserve(port=self.args.prober_port)
-            
             self.flask_ports, self.comm_ports = [], []
+            self.worker_addrs = []
             for _ in range(self.args.num_workers):
                 _, flask_port = sset.reserve()
                 self.flask_ports.append(flask_port)
                 _, comm_port = sset.reserve()
                 self.comm_ports.append(comm_port)
+                self.worker_addrs.append(f"localhost:{flask_port}")
 
     def create_workers(self):
         self.workers = []
@@ -83,7 +80,7 @@ class WithLeader:
 
         self.gateway_conf.write(self.nginx_conf_j2.render(
             gateway_port=self.args.gateway_port,
-            leader=None,
+            worker_addrs=self.worker_addrs,
         ))
         self.gateway_conf.close()
 
@@ -95,31 +92,12 @@ class WithLeader:
     def update_gateway(self, leader):
         conf_txt = self.nginx_conf_j2.render(
             gateway_port=self.args.gateway_port,
-            leader=leader
+            worker_addrs=self.worker_addrs,
         )
         with open(self.gateway_conf.name, "w") as conf_f:
             conf_f.write(conf_txt)
 
         os.kill(self.gateway.pid, signal.SIGHUP)
-    
-    def create_flask_app(self):
-        app = Flask(__name__)
-
-        class UpdateLeaderSchema(Schema):
-            leader = fields.Str()
-
-        @app.put("/leader")
-        def update_leader():
-            data = UpdateLeaderSchema().load(request.json)
-            self.update_gateway(data["leader"])
-            return {}
-        
-        def flask_proc_fn():
-            sys.stdout = open(os.devnull, "w")
-            app.run(use_reloader=False, debug=False, port=self.flask_port)
-        
-        self.flask_srv = Process(target=flask_proc_fn)
-        self.flask_srv.start()
     
     def get_delay_rv(self, params: List[float]):
         if len(params) > 1:
@@ -139,35 +117,16 @@ class WithLeader:
         
         self.killer = Killer(self.workers, self.finishing, kill_every, restart_after)
         self.killer.start()
-
-    def create_prober(self):
-        prober_argv = ["python3", "-m", "paxos.prober"]
-        prober_argv.extend(["--probe-period", str(self.args.probe_period)])
-        prober_argv.extend(["--port", str(self.prober_port)])
-        prober_argv.extend(["--worker-ports", *(str(p) for p in self.flask_ports)])
-        if self.args.gateway_port is not None:
-            leader_update_cb = f"http://localhost:{self.flask_port}/leader"
-            prober_argv.extend(["--leader-update-cb", leader_update_cb])
-        if self.args.verbose:
-            prober_argv.extend(["-v"])
-
-        self.prober = subprocess.Popen(prober_argv, stdout=DEVNULL, stdin=DEVNULL)
-
+    
     def setup_signals(self):
         def handler(signo, frame):
             self.finishing.set()
         
         for sig in (signal.SIGINT, signal.SIGTERM):
             signal.signal(sig, handler)
-
+    
     def cleanup(self):
-        self.prober.terminate()
-        self.prober.wait()
-
         if self.args.gateway_port is not None:
-            self.flask_srv.terminate()
-            self.flask_srv.join()
-
             self.gateway.terminate()
             self.gateway.wait()
         
@@ -176,7 +135,7 @@ class WithLeader:
         
         for worker in self.workers:
             worker.kill()
-
+    
     def main(self):
         self.finishing = threading.Event()
 
@@ -184,14 +143,11 @@ class WithLeader:
         self.setup_logging()
 
         self.reserve_ports()
-
-        if self.args.gateway_port is not None:
-            self.create_gateway()
-            self.create_flask_app()
         
         self.create_workers()
 
-        self.create_prober()
+        if self.args.gateway_port is not None:
+            self.create_gateway()
 
         if self.args.kill_every is not None:
             self.create_killer()
@@ -202,6 +158,7 @@ class WithLeader:
 
         self.cleanup()
 
+
 if __name__ == "__main__":
-    app = WithLeader()
-    app.main() 
+    app = Leaderless()
+    app.main()
