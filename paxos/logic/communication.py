b2e0a5cf5464fef81c8c4e1c182d7c6c8a8db47f
quczer <kucharczi@gmail.com>
2023-01-19 00:20:42 +0100

    comm minors

--- paxos/logic/communication.py
+++ paxos/logic/communication.py
@@ -54,24 +54,23 @@ class Network:
     me: Node
 
     @staticmethod
+    def get_uids(addrs: Iterable[Address]) -> dict[Address, int]:
+        addr_to_id = {addr: idx for idx, addr in enumerate(sorted(addrs))}
+        return addr_to_id
+
+    @staticmethod
     def from_addresses(addrs: Iterable[Address], addr: Address) -> Network:
         """Construct a Paxos network from the addresses of the nodes.
         :param addrs: Addresses of all the nodes in the network.
         :param addr: Address of the calling process in the network."""
 
-        addrs_ids = [(addr, idx) for idx, addr in enumerate(addrs)]
-        addrs_ids = sorted(addrs_ids, key=lambda x: x[0])
-
-        my_id = None
-        for node_addr, node_id in addrs_ids:
-            if addr == node_addr:
-                my_id = node_id
-                break
+        addrs_ids = Network.get_uids(addrs)
+        my_id = addrs_ids.get(addr)
 
         assert my_id is not None
 
         all_roles = {Role.ACCEPTOR, Role.LEARNER, Role.PROPOSER}
-        nodes = {uid: Node(uid, addr, all_roles) for addr, uid in addrs_ids}
+        nodes = {uid: Node(uid, addr, all_roles) for addr, uid in addrs_ids.items()}
         return Network(nodes, nodes[my_id])
 
     def all_of(self, role: Role) -> Iterable[Node]:
